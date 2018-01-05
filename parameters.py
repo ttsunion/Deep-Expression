38df14fbd833124d13ca00020e6bf96e3841d213
Shuming Fang <fangshuming519@gmail.com>
2018-01-05 09:38:06 +0800

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -8,7 +8,10 @@ class params:
     # data
     max_duration = 10.0 # seconds
     Tx = 20 # characters. maximum length of text.
-
+    frame_length = 0.05 # seconds
+    Ty = int(sr * frame_length) # signals. maximum length  of signal.
+    Dy = int(sr * max_duration/Ty)
     # training scheme
     lr = 0.001
+    batch_size = 4
     logdir = "logdir"
