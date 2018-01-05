0d60701759ae047b5594fa9cbbcb4cff60ca39b1
Shuming Fang <fangshuming519@gmail.com>
2018-01-05 10:04:15 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,17 @@
 import numpy as np
 import tensorflow as tf
+import os
 from text import *
 from parameters import params as pm
+
 graph = tf.Graph
+labels = []
+with open('samples/labels/labels.txt', 'r', encoding = 'utf-8') as lb:
+    for l in lb.readlines():
+        l = l.strip()
+        l = l.split('||')[1]
+        labels.append(text2label(l))
+labels = np.array(labels)
 with graph.as_default():
     x = tf.placeholder("float32", [pm.batch_size, pm.Tx], name = "x")
     y = tf.placeholder("int32", [pm.batch_size, pm.Dy, pm.Ty], name = "y")
