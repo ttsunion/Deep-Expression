5d44ca879d99f5da760b25013816d652140496f3
Shuming Fang <fangshuming519@gmail.com>
2018-01-10 17:53:35 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 import os
+os.chdir('E:\\Deep-Express-master')
 from scipy.io import wavfile
 from text import *
 from parameters import params as pm
@@ -53,7 +54,7 @@ y = tf.placeholder("float32", [pm.batch_size, pm.Dy, pm.Ty], name = "y")
 lookup_table = tf.Variable(tf.random_uniform((pm.vocab_size, pm.num_units), minval=-0.5, maxval=0.5,dtype=tf.float32), name = 'lookup_table')
 x_embed = tf.nn.embedding_lookup(lookup_table, x, name = 'x_embed')
 x_embed += positional_encoding(x, pm.num_units)
-w1 = tf.truncated_normal((3, pm.num_units, pm.num_units), mean=0.0, stddev=1000.0, dtype=tf.float32, seed=None, name='w1')
+w1 = tf.truncated_normal((3, pm.num_units, pm.num_units), mean=0.0, stddev=0.01, dtype=tf.float32, seed=None, name='w1')
 Q = feed_forward(x_embed, w1[0, :, :])
 K = feed_forward(x_embed, w1[1, :, :])
 V = feed_forward(x_embed, w1[2, :, :])
@@ -61,11 +62,11 @@ net = tf.matmul(Q, tf.transpose(K, [0, 2, 1]))
 net = tf.matmul(net, V)
 net += x_embed
 net = normalize(net)
-w2 = tf.tile(tf.truncated_normal((pm.step_size, pm.num_units), mean=0.0, stddev=1000.0, dtype=tf.float32, seed=None), [int(pm.num_units/pm.step_size), 1], name = 'w2')
-w3 = tf.tile(tf.truncated_normal((pm.step_size, pm.num_units), mean=0.0, stddev=1000.0, dtype=tf.float32, seed=None), [int(pm.num_units/pm.step_size), 1], name = 'w3')
-w4 = tf.tile(tf.truncated_normal((pm.step_size, pm.num_units), mean=0.0, stddev=1000.0, dtype=tf.float32, seed=None), [int(pm.num_units/pm.step_size), 1], name = 'w4')
-w5 = tf.tile(tf.truncated_normal((pm.step_size, pm.num_units), mean=0.0, stddev=1000.0, dtype=tf.float32, seed=None), [int(pm.num_units/pm.step_size), 1], name = 'w5')
-w6 = tf.tile(tf.truncated_normal((pm.step_size, pm.num_units), mean=0.0, stddev=1000.0, dtype=tf.float32, seed=None), [int(pm.num_units/pm.step_size), 1], name = 'w6')
+w2 = tf.tile(tf.truncated_normal((pm.step_size, pm.num_units), mean=0.0, stddev=0.01, dtype=tf.float32, seed=None), [int(pm.num_units/pm.step_size), 1], name = 'w2')
+w3 = tf.tile(tf.truncated_normal((pm.step_size, pm.num_units), mean=0.0, stddev=0.01, dtype=tf.float32, seed=None), [int(pm.num_units/pm.step_size), 1], name = 'w3')
+w4 = tf.tile(tf.truncated_normal((pm.step_size, pm.num_units), mean=0.0, stddev=0.01, dtype=tf.float32, seed=None), [int(pm.num_units/pm.step_size), 1], name = 'w4')
+w5 = tf.tile(tf.truncated_normal((pm.step_size, pm.num_units), mean=0.0, stddev=0.01, dtype=tf.float32, seed=None), [int(pm.num_units/pm.step_size), 1], name = 'w5')
+w6 = tf.tile(tf.truncated_normal((pm.step_size, pm.num_units), mean=0.0, stddev=0.01, dtype=tf.float32, seed=None), [int(pm.num_units/pm.step_size), 1], name = 'w6')
 net += feed_forward(net, w2)
 net = tf.nn.relu(net)
 net = normalize(net)
@@ -81,23 +82,23 @@ net = normalize(net)
 net += feed_forward(net, w6)
 net = tf.nn.relu(net)
 net = normalize(net)
-w7 = tf.tile(tf.truncated_normal((1, pm.Dy,), mean=0.0, stddev=1000.0, dtype=tf.float32, seed=None), [pm.num_units, 1], name = 'w7')
+w7 = tf.tile(tf.truncated_normal((1, pm.Dy,), mean=0.0, stddev=0.01, dtype=tf.float32, seed=None), [pm.num_units, 1], name = 'w7')
 net = feed_forward(net, w7)
 net = normalize(net)
 net = tf.transpose(net, [0, 2, 1])
-w8 = tf.truncated_normal((pm.Tx, pm.Ty), mean=0.0, stddev=1000.0, dtype=tf.float32, seed=None, name = 'w8')
+w8 = tf.truncated_normal((pm.Tx, pm.Ty), mean=0.0, stddev=0.01, dtype=tf.float32, seed=None, name = 'w8')
 yhat = feed_forward(net, w8)
 loss = tf.reduce_mean(tf.abs(y - yhat), name = 'loss')
 optimizer = tf.train.AdamOptimizer(learning_rate = pm.lr).minimize(loss)
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    for i in range(500):
+    for i in range(2000):
         _ = sess.run(optimizer, feed_dict = {x:labels, y:wavs})
         print('Step: ', i, 'loss: ', sess.run(loss, feed_dict = {x:labels, y:wavs}))
         if i % 100 == 0:
             ypred = sess.run(yhat, feed_dict = {x:labels, y:wavs})
-            ypred = ypred[0, :, :]
-            ypred = ypred.reshape(1, -1)[0]
+            ypred = ypred[0, :, :] 
+            ypred = ypred.reshape(1, -1)[0] * 10000
             ypred = ypred.astype(np.int16)
             wavfile.write('output.wav', 16000, ypred)
 plt.plot(ypred)
