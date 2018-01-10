49c28a989424489edac9d451932cab34f08bc67d
Shuming Fang <fangshuming519@gmail.com>
2018-01-10 16:53:13 +0800

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -13,6 +13,7 @@ class params:
     Ty = int(sr * frame_length) # signals. maximum length  of signal.
     Dy = int(sr * max_duration/Ty)
     num_filters = 2048
+    step_size = 4
     # training scheme
     lr = 0.005
     batch_size = 4
